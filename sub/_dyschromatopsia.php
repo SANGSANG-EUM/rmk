3c427ffcfe65688d4c33b3271fc571d1087c8a46
hosu <qxvop@iium.kr>
2024-03-06 15:47:06 +0900

    [서브] 색각이상 페이지 작업 완료

---
+++ sub/_dyschromatopsia.php
@@ -0,0 +1,213 @@
+<div id="dys" class="sub">
+  <div class="sub-head">
+    <div class="wrapper">
+      <h1 class="sub-head_title">색각이상이란?</h1>
+    </div>
+  </div>
+  <div class="sub-body">
+
+    <section class="dys-visual sub-visual">
+      <div class="wrapper">
+        <div class="dys-visual-txt">
+          <img src="/src/img/dys-vsicon.svg" alt="">
+          <p>색각이상은 3가지의 망막 색소(적색, 녹색, 청색)중 하나가 감소하거나 변질 또는 결핍이 원인으로 색분별 이상이 발생하는 것을 말합니다.</p>
+        </div>
+      </div>
+    </section>
+
+    <section class="dys-overview sub-content">
+      <div class="wrapper">
+        <p class="content_title1">색을 구분하지 못해 <br> 불편했던 경험 있으신가요?</p>
+        <p class="content_text">보건복지부 자료에 따르면 현재 국내에서 색각이상을 겪고 있는 분들은 총 165만명입니다. <br> 이 분들은 일상 속 의도치 않은 불편함 속에 노출되어
+          있습니다.</p>
+        <ul class="dys-overview-ul">
+          <li class="dys-overivew-li">
+            <p class="dys-sec-tit">선천성 색각이상</p>
+            <div class="dys-overview-famtree">
+              <img src="/src/img/dys-famtree.png" alt="선천성 색각이상 가계도 이미지">
+            </div>
+          </li>
+          <li class="dys-overivew-li">
+            <p class="dys-sec-tit">남/여 색각이상자 비율</p>
+            <div class="dys-overview-graph-wr">
+              <div class="dys-overview-graph dys-overview-graph--male">
+                <div class="dys-overview-circle">
+                  <div class="dys-overview-percentage">
+                  </div>
+                  <div class="dys-overview-in">
+                    <img src="/src/img/dys-male.svg" alt="">
+                    <p>남성</p>
+                  </div>
+                </div>
+                <p class="dys-overview-info">
+                  12명 중 1명
+                </p>
+              </div>
+              <div class="dys-overview-graph dys-overview-graph--female">
+                <div class="dys-overview-circle">
+                  <div class="dys-overview-percentage">
+                  </div>
+                  <div class="dys-overview-in">
+                    <img src="/src/img/dys-female.svg" alt="">
+                    <p>여성</p>
+                  </div>
+                </div>
+                <p class="dys-overview-info">
+                  200명 중 1명
+                </p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="dys-experience sub-content">
+      <div class="wrapper">
+        <p class="dys-sec-tit">색각이상이 있을 경우 이런 불편함이 있어요.</p>
+        <ul class="dys-exp1-ul">
+          <li class="dys-exp1-li">
+            <div class="dys-exp1-li__icon">
+              <img src="/src/img/dys-icon1.svg" alt="">
+            </div>
+            <div class="dys-exp-txt-wr">
+              <p class="dys-exp-txt dys-exp-txt--01">고3 수험생</p>
+              <p class="dys-exp-txt dys-exp-txt--02">검은펜을 쓰는 줄 알았는데 자신도 모르게 빨간펜을 사용하는 사례 발생</p>
+            </div>
+          </li>
+          <li class="dys-exp1-li">
+            <div class="dys-exp1-li__icon">
+              <img src="/src/img/dys-icon2.svg" alt="">
+            </div>
+            <div class="dys-exp-txt-wr">
+              <p class="dys-exp-txt dys-exp-txt--01">현대사회 회사원</p>
+              <p class="dys-exp-txt dys-exp-txt--02">문서 작업 시 색상 구분이 어려워 잔 실수, 똑같은 행동 반복하는 사례 발생</p>
+            </div>
+          </li>
+          <li class="dys-exp1-li">
+            <div class="dys-exp1-li__icon">
+              <img src="/src/img/dys-icon3.svg" alt="">
+            </div>
+            <div class="dys-exp-txt-wr">
+              <p class="dys-exp-txt dys-exp-txt--01">디자이너</p>
+              <p class="dys-exp-txt dys-exp-txt--02">색구분이 힘들어서 무채색으로 디자인하고 창의적인 작품에 대해 제한되는 사례 발생</p>
+            </div>
+          </li>
+          <li class="dys-exp1-li">
+            <div class="dys-exp1-li__icon">
+              <img src="/src/img/dys-icon4.svg" alt="">
+            </div>
+            <div class="dys-exp-txt-wr">
+              <p class="dys-exp-txt dys-exp-txt--01">대학생</p>
+              <p class="dys-exp-txt dys-exp-txt--02">가끔 양말색을 짝짝이로 신어서, 지금은 한가지 색상 양말만 신는 사례 발생</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div class="dys-exp2-bg">
+        <div class="wrapper">
+          <ul class="dys-exp2-ul">
+            <li class="dys-exp2-li">
+              <div class="dys-exp2-li__img dys-exp2-li__img--01">
+                <img src="/src/img/dys-img1.jpg" alt="">
+              </div>
+              <div class="dys-exp-txt-wr">
+                <p class="dys-exp-txt dys-exp-txt--01">색약에 제한이 있는 직업군</p>
+                <p class="dys-exp-txt dys-exp-txt--02">의사, 화가, 경찰, 비행사, 운전사 등 색상 구분이 필요한 경우 제한됨</p>
+              </div>
+            </li>
+            <li class="dys-exp2-li">
+              <div class="dys-exp2-li__img dys-exp2-li__img--02">
+                <img src="/src/img/dys-img2.jpg" alt="">
+              </div>
+              <div class="dys-exp-txt-wr">
+                <p class="dys-exp-txt dys-exp-txt--01">야간 신호등 색상 구분</p>
+                <p class="dys-exp-txt dys-exp-txt--02">야간 운전 시 적색, 녹색 신호의 구분이 어려워 운전시 위험함</p>
+              </div>
+            </li>
+            <li class="dys-exp2-li">
+              <div class="dys-exp2-li__img dys-exp2-li__img--03">
+                <img src="/src/img/dys-img3.jpg" alt="">
+              </div>
+              <div class="dys-exp-txt-wr">
+                <p class="dys-exp-txt dys-exp-txt--01">일반인들의 선입견</p>
+                <p class="dys-exp-txt dys-exp-txt--02">색약임을 밝힌 유명 방송인의 경우처럼 일반인들에게 선입견을 받을 수 있음</p>
+              </div>
+            </li>
+            <li class="dys-exp2-li">
+              <div class="dys-exp2-li__img dys-exp2-li__img--04">
+                <img src="/src/img/dys-img4.jpg" alt="">
+              </div>
+              <div class="dys-exp-txt-wr">
+                <p class="dys-exp-txt dys-exp-txt--01">글씨 색상 구분</p>
+                <p class="dys-exp-txt dys-exp-txt--02">각종 문서 작업시 중요 지침 구분이 어려울 수 있음</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+
+    <section class="dys-sunglasses sub-content">
+      <div class="wrapper">
+        <p class="content_title1">색상왜곡, 불편함은 그만! <br> 더 많은 색을 느끼다.</p>
+        <p class="content_text">적록색약의 경우 붉은색, 노란색, 초록색 계열 색상 구분이 어렵습니다. <br> 돌튼은 기존 외국 제품과 달리 염료가 아닌 광학필터를 사용하여 색상왜곡을
+          최소화하며 색 구분을 도와줍니다.</p>
+        <div class="dys-sunglasses-wr">
+          <div class="dys-sunglasses-l">
+            <img src="/src/img/dys-btm1.jpg" alt="">
+          </div>
+          <div class="dys-sunglasses-r">
+            <div class="dys-sunglassses-img">
+              <img src="/src/img/dys-btm2.jpg" alt="">
+            </div>
+            <div class="dys-exp-txt-wr">
+              <p class="dys-exp-txt dys-exp-txt--01">돌튼 색약보정 선글라스 <br> 지금 차이를 느껴보세요!</p>
+              <p class="dys-exp-txt dys-exp-txt--02">돌튼이 선사하는 새로운 시선으로 불편함을 없애고, <br> 일상 속 편리함을 더해 좀 더 나은 오늘을 두 눈에
+                담아보세요.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+  </div>
+</div>
+
+<script>
+  // 스크롤 이벤트 리스너 추가
+  $(window).on('scroll', function () {
+    // 그래프가 있는 요소의 위치 확인
+    var graphPosition = $('.dys-overview-graph--male').offset().top;
+
+    // 현재 스크롤 위치 확인
+    var scrollPosition = $(window).scrollTop() + $(window).height();
+
+    // 그래프가 보이는 부분까지 스크롤했을 때 스크립트 실행
+    if (scrollPosition > graphPosition) {
+      draw(15, '.dys-overview-graph--male .dys-overview-percentage', '#52A2C6');
+      draw(3, '.dys-overview-graph--female .dys-overview-percentage', '#E5685C');
+
+      // 스크롤 이벤트 제거 (스크립트가 한 번만 실행되도록)
+      $(window).off('scroll');
+    }
+  });
+
+  function draw(max, classname, colorname) {
+    var i = 1;
+    var func1 = setInterval(function () {
+      if (i < max) {
+        color1(i, classname, colorname);
+        i++;
+      } else {
+        clearInterval(func1);
+      }
+    }, 40);
+  }
+
+  function color1(i, classname, colorname) {
+    $(classname).css({
+      "background": "conic-gradient(" + colorname + " 0% " + (i - 0.1) + "%, #f0f0f0 " + i + "% 100%)"
+    });
+  }
+</script>
