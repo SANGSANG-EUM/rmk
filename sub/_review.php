aa40ff6c97bcc0a61cdbde662c03fc78f4f3cb31
구승연 <gu@iium.kr>
2024-03-06 16:30:22 +0900

    [서브] 고객리뷰 페이지 작업 중(보류)

---
+++ sub/_review.php
@@ -0,0 +1,39 @@
+<div id="review" class="sub">
+  <div class="sub-head">
+    <div class="wrapper">
+      <h1 class="sub-head_title">고객 리뷰</h1>
+    </div>
+  </div>
+  <div class="sub-body">
+    <section class="review-best sub-content">
+      <div class="review-best_slider swiper-container">
+        <div class="review-best_slide swiper-slide">
+          <div class="review-best_thumb">
+            <img src="" alt="">
+          </div>
+          <div class="review-best_content">
+            <div class="review-best_score"></div>
+            <div class="review-best_tag">
+              <p><b>프레임</b> 금속테</p>
+              <p><b>렌즈</b> 적색(고기능)</p>
+            </div>
+            <p class="review-best_text">리뷰 내용이 표시됩니다. 리뷰 내용이 표시됩니다. 리뷰 내용이 표시됩니다. 리뷰 내용이 표시됩니다. 리뷰 내용이 표시됩니다. 리뷰 내용이 표시됩니다. 리뷰 내용이 표시됩니다. 리뷰 내용이 표시됩니다. 리뷰 내용이 표시됩니다. 리뷰 내용이 표시됩니다. 리뷰 내용이 표시됩니..</p>
+            <div class="review-best_eval">
+              <p>무게 적당해요</p>
+              <p>마감처리 깔끔해요</p>
+              <p>착용감 편해요</p>
+            </div>
+            <p class="review-best_name">abcd12**</p>
+          </div>
+        </div>
+      </div>
+    </section> 
+    <section class="review-content sub-content">
+      <div class="wrapper">
+        <p class="content_title2">알엠케이 사용자들의 다양하고 솔직한 <br/>리뷰를 확인해보세요.</p>
+        <div class="review-list"></div>
+      </div>
+    </section>
+    </section>
+  </div>
+</div>
