cb58b7f9a248ff76ffceab90816dfb898666cfbf
onesick <onesick@iium.kr>
2024-03-06 16:33:26 +0900

    [메인] 돌튼 콘텐츠 PC 완료

--- theme/basic/index.php
+++ theme/basic/index.php
@@ -89,7 +89,9 @@ include_once(G5_THEME_PATH.'/head.php');
                 <img src="/src/img/dalton-text-d.svg" class="dalton-text-d" alt="d">
                 <img src="/src/img/dalton-text.svg" class="dalton-text" alt="dalton">
               </div>
+
               <p class="content-text">알엠케이의 <b>색상 보정 안경 브랜드 ‘돌튼’</b>은 <br/><b>돌터니즘</b>의 <b>John Dalton</b>에서 탄생했습니다.</p>
+
               <div class="dalton_proud">
                 <div class="round-right  dalton_proud-item">
                   <div class="icon"></div>
@@ -107,12 +109,59 @@ include_once(G5_THEME_PATH.'/head.php');
                   <div class="t2">대중적인 선글라스로서 누구나 <br/>컬러 선글라스를 즐길 수 있습니다.</div>
                 </div>
               </div>
+
+              <div class="dalton_exp">
+                <p class="content_title1">돌튼의 효과를 경험하고 싶지만 <br/>외출이 힘드신가요?</p>
+                <p class="content-text3"><b>착용 전후 사진</b>을 아래에서 비교해보시고, <br/>이전에 느끼지 못했던 <b>경험을 누리세요.</b></p>
+
+                <div class="dalton_exp-beforeAfter">
+                  <div class="background-img img"></div>
+                  <div class="foreground-img img"></div>
+                  <input type="range" min="0" max="100" value="50" class="slider" name="dalton_exp-beforeAfter-slider" id="dalton_exp-beforeAfter-slider">
+                  <div class="slider-button">
+                    <span class="slider-circle"></span>
+                  </div>
+                </div>
+
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+
+  <div class="main_sec main_product">
+    <div class="main_sec-top">
+      <div class="container">
+        <div class="wrapper">
+          <div class="inner">
+            <p class="content_title1">색약보정 아이웨어 돌튼의 제품은 <br/>색을 선물해드립니다.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="main_sec-ct">
+      <div class="container">
+        <div class="wrapper">
+          <div class="inner">
+            <div class="main_product-type">
+              <div class="main_product-type-item item1">
+                <img src="/src/img/main-dalton-img1.jpg" alt="">
+              </div>
+              <div class="round-top main_product-type-item item2">
+                <img src="/src/img/main-dalton-img2.jpg" alt="">
+              </div>
+              <div class="round main_product-type-item item3">
+                <img src="/src/img/main-dalton-img3.jpg" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </div>
 <!-- } main -->
 
