ba68903a2b6ac15a85bc6ff8a0751cc9e33b4633
구승연 <gu@iium.kr>
2024-03-07 13:11:50 +0900

    [서브] 주요연혁 페이지 작업 완료(PC)

---
+++ sub/_history.php
@@ -0,0 +1,73 @@
+<div id="history" class="sub">
+  <div class="sub-head">
+    <div class="wrapper">
+      <h1 class="sub-head_title">주요 연혁</h1>
+    </div>
+  </div>
+  <div class="sub-body">
+    <section class="history-visual sub-visual"></section>
+    <section class="history-content sub-content">
+      <div class="wrapper">
+        <p class="content_title1">History of RMK</p>
+        <div class="history-box">
+          <div class="history-box_row">
+            <div class="history-box_year history-box_item">2024</div>
+            <div class="history-box_text history-box_item">
+              <p>미국특허등록 (등록번호 11,927,778)</p>
+            </div>
+          </div>
+          <div class="history-box_row">
+            <div class="history-box_year history-box_item">2023</div>
+            <div class="history-box_text history-box_item">
+              <p>삼성문화재단mou체결 (리움·호암미술관 색약안경 공급)</p>
+            </div>
+          </div>
+          <div class="history-box_row">
+            <div class="history-box_year history-box_item">2022</div>
+            <div class="history-box_text history-box_item">
+              <p>서울국제발명전시회금상 수상</p>
+              <p>(예비) 사회적기업 지정만료</p>
+              <p>색약보정선글라스 수출 개시 (일본)</p>
+              <p>색약보정선글라스 출시</p>
+            </div>
+          </div>
+          <div class="history-box_row">
+            <div class="history-box_year history-box_item">2021</div>
+            <div class="history-box_text history-box_item">
+              <p>색약보정안경렌즈 의료기기 인증 (식품의약품안전처)</p>
+            </div>
+          </div>
+          <div class="history-box_row">
+            <div class="history-box_year history-box_item">2020</div>
+            <div class="history-box_text history-box_item">
+              <p>벤처기업인증 (제20200301008호)</p>
+              <p>한국벤처투자㈜1억원 투자유치</p>
+              <p>의료기기제조업 허가 (제6790호)</p>
+            </div>
+          </div>
+          <div class="history-box_row">
+            <div class="history-box_year history-box_item">2019</div>
+            <div class="history-box_text history-box_item">
+              <p>소셜벤쳐 판별 (기술보증기금 소셜벤쳐 가치평가센터)</p>
+              <p>(예비) 사회적기업 지정 (세종 제2019-28호)</p>
+              <p>청년창업사관학교졸업 (창업성공패키지, 중소기업진흥공단)</p>
+            </div>
+          </div>
+          <div class="history-box_row">
+            <div class="history-box_year history-box_item">2018</div>
+            <div class="history-box_text history-box_item">
+              <p><b>07.23</b>주식회사알엠케이 법인 설립</p>
+              <p>최초특허 출원 – (IP 디딤돌 사업 수행, 특허청)</p>
+            </div>
+          </div>
+          <div class="history-box_row">
+            <div class="history-box_year history-box_item">2017</div>
+            <div class="history-box_text history-box_item">
+              <p>색각이상 아이템 사업화 기획</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
