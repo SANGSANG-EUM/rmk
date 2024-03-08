6302f294f8978dceb5c2efc60e097b3df2b46394
hosu <qxvop@iium.kr>
2024-03-08 13:13:55 +0900

    [서브] 차별화기술 페이지 작업 완료 (PC)

---
+++ sub/_difference.php
@@ -0,0 +1,196 @@
+<div id="difference" class="sub">
+  <div class="sub-head">
+    <div class="wrapper">
+      <h1 class="sub-head_title">차별화 기술</h1>
+    </div>
+  </div>
+  <div class="sub-body">
+    <section class="difference-visual sub-visual"></section>
+    <section class="difference-overview sub-content">
+      <div class="wrapper">
+        <p class="content_title1">첨단 광학기술 적용된 선글라스 <br> ‘돌튼 선글라스 기술력’</p>
+        <div class="difference-overview-wr">
+          <div class="difference-overview-txt-wr">
+            <p class="difference-overview-txt1 guide-info-tit">알엠케이의 우수한 기술력</p>
+            <p class="difference-overview-txt2 sub-mid-tit">돌튼은 현 세계에서 유일하게 광구조방식 컬러필터를 사용한 색약안경입니다.</p>
+            <p class="difference-overview-txt3 guide-info-txt">알엠케이는 미국 특허 및 다수의 국내 특허 보유를 보유하고 있으며, 2022년 서울국제발명전시회 금상 수상 등 <b>세계 최고의 색약안경 기술력</b>을 가지고 있습니다.</p>
+          </div>
+          <div class="difference-img">
+            <img src="/src/img/difference-overview.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="difference-cert sub-content">
+      <div class="wrapper">
+        <div class="difference-cert-wr match_h">
+          <div class="difference-cert-big">
+            <img src="/src/img/difference-cert1.jpg" alt="">
+          </div>
+          <ul class="difference-cert-ul">
+            <li class="difference-cert-li">
+              <img src="/src/img/difference-cert2.jpg" alt="">
+            </li>
+            <li class="difference-cert-li">
+              <img src="/src/img/difference-cert3.jpg" alt="">
+            </li>
+            <li class="difference-cert-li">
+              <img src="/src/img/difference-cert4.jpg" alt="">
+            </li>
+            <li class="difference-cert-li">
+              <img src="/src/img/difference-cert5.jpg" alt="">
+            </li>
+            <li class="difference-cert-li">
+              <img src="/src/img/difference-cert6.jpg" alt="">
+            </li>
+            <li class="difference-cert-li">
+              <img src="/src/img/difference-cert7.jpg" alt="">
+            </li>
+            <li class="difference-cert-li">
+              <img src="/src/img/difference-cert8.jpg" alt="">
+            </li>
+            <li class="difference-cert-li">
+              <img src="/src/img/difference-cert9.jpg" alt="">
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section class="difference-filter sub-content">
+      <div class="wrapper">
+        <p class="content_title1">광구조방식 컬러필터란?</p>
+        <p class="content_text">첨단 광학설계를 통해 초록색 파장만 차단하는 필터를 적용해 색구분을 가능하게 하는 방식으로 <br> ‘색구분 성능은 높이고 색상 왜곡은 최소화’하는 기술입니다.</p>
+        <div class="difference-filter-wr">
+          <div class="difference-filter-img">
+            <img src="/src/img/difference-filter0.jpg" alt="">
+          </div>
+          <ul class="difference-filter-ul">
+            <li class="difference-filter-li">
+              <p class="difference-filter-li__tit sub-mid-tit">일반 색각</p>
+              <div class="difference-filter-li__img">
+                <img src="/src/img/difference-filter1.jpg" alt="">
+              </div>
+              <p class="difference-filter-li__txt guide-info-tit">일반 색상 구분</p>
+            </li>
+            <li class="difference-filter-li">
+              <p class="difference-filter-li__tit sub-mid-tit">제1 색맹</p>
+              <div class="difference-filter-li__img">
+                <img src="/src/img/difference-filter2.jpg" alt="">
+              </div>
+              <p class="difference-filter-li__txt guide-info-tit">두 색상의 구분이 힘듦</p>
+            </li>
+            <li class="difference-filter-li">
+              <p class="difference-filter-li__ti sub-mid-tit">돌튼 착용</p>
+              <div class="difference-filter-li__img">
+                <img src="/src/img/difference-filter3.jpg" alt="">
+              </div>
+              <p class="difference-filter-li__txt guide-info-tit">두 색상의 구분이 가능</p>
+            </li>
+          </ul>
+          <div class="difference-filter-txt-wr">
+            <p class="guide-info-tit">돌튼 렌즈는 녹색 영역의 일부를 투과율  5% 이하로 차단하는 기능이 핵심입니다.</p>
+            <p class="guide-info-txt">일반적으로 제1 제2 색각이상의 경우는 대비를 강조하여 적색과 녹색을 구분할 수 있습니다. <br> 또 다른 경우 중에 녹색 빛에 과민한 색각이상인 경우는 녹색을 감지하는 시세포의 활성도를 감소시켜 색각이상을 개선할 수 있습니다 <br> <span>(*돌튼의 색보정 원리를 설명하기 위해 가공된 이미지로 실제 시각과 차이가 있습니다.)</span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="difference-compare sub-content">
+      <div class="wrapper">
+        <p class="content_title1">타사 제품과 비교</p>
+        <p class="content_text">외국 타사 제품과 비교해도 떨어지지 않는 기술력과 제품에 대한 품질을 자부합니다.</p>
+        <ul class="difference-compare-ul">
+          <li class="difference-compare-li">
+            <p class="difference-compare-tit sub-mid-tit">RMK (자사)</p>
+            <div class="difference-compare-box">
+              <div class="difference-compare-img">
+                <img src="/src/img/difference-sunglasses1.png" alt="">
+              </div>
+              <ul class="difference-compare-txt-ul">
+                <li class="difference-comapre-txt-li">
+                  <p class="difference-comapre-txt-li__key">개발국</p>
+                  <p class="difference-comapre-txt-li__val">순수 국산 기술</p>
+                </li>
+                <li class="difference-comapre-txt-li">
+                  <p class="difference-comapre-txt-li__key">색보정 방식</p>
+                  <p class="difference-comapre-txt-li__val">광구조 컬러필터 방식</p>
+                </li>
+                <li class="difference-comapre-txt-li">
+                  <p class="difference-comapre-txt-li__key">색보정 성능</p>
+                  <p class="difference-comapre-txt-li__val">우수</p>
+                </li>
+                <li class="difference-comapre-txt-li">
+                  <p class="difference-comapre-txt-li__key">가격대</p>
+                  <p class="difference-comapre-txt-li__val">중간</p>
+                </li>
+                <li class="difference-comapre-txt-li">
+                  <p class="difference-comapre-txt-li__key">구입처</p>
+                  <p class="difference-comapre-txt-li__val">온라인 구입 가능(선글라스)</p>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="difference-compare-li">
+            <p class="difference-compare-tit sub-mid-tit">A사 (타사)</p>
+            <div class="difference-compare-box">
+              <div class="difference-compare-img">
+                <img src="/src/img/difference-sunglasses2.png" alt="">
+              </div>
+              <ul class="difference-compare-txt-ul">
+                <li class="difference-comapre-txt-li">
+                  <p class="difference-comapre-txt-li__key">개발국</p>
+                  <p class="difference-comapre-txt-li__val">영국</p>
+                </li>
+                <li class="difference-comapre-txt-li">
+                  <p class="difference-comapre-txt-li__key">색보정 방식</p>
+                  <p class="difference-comapre-txt-li__val">염료방식</p>
+                </li>
+                <li class="difference-comapre-txt-li">
+                  <p class="difference-comapre-txt-li__key">색보정 성능</p>
+                  <p class="difference-comapre-txt-li__val">우수</p>
+                </li>
+                <li class="difference-comapre-txt-li">
+                  <p class="difference-comapre-txt-li__key">가격대</p>
+                  <p class="difference-comapre-txt-li__val">비쌈</p>
+                </li>
+                <li class="difference-comapre-txt-li">
+                  <p class="difference-comapre-txt-li__key">구입처</p>
+                  <p class="difference-comapre-txt-li__val">전문안경원에서만 구입 가능</p>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <section class="difference-color sub-content">
+      <div class="wrapper">
+        <p class="content_title1">제품 착용 후 색상비교</p>
+        <p class="content_text">RMK 돌튼 제품 착용 시 실제 보여지는 색상을 참고해주세요.</p>
+        <ul class="difference-color-ul">
+          <li class="difference-color-li">
+            <p class="difference-color-li__tit sub-mid-tit">녹색 렌즈 착용 시</p>
+            <div class="difference-color-li__img-wr">
+              <div class="difference-color-li__img">
+                <img src="/src/img/difference-green1.jpg" alt="">
+              </div>
+              <div class="difference-color-li__img">
+                <img src="/src/img/difference-green2.jpg" alt="">
+              </div>
+            </div>
+          </li>
+          <li class="difference-color-li">
+            <p class="difference-color-li__tit sub-mid-tit">적색 렌즈 착용 시</p>
+            <div class="difference-color-li__img-wr">
+              <div class="difference-color-li__img">
+                <img src="/src/img/difference-red1.jpg" alt="">
+              </div>
+              <div class="difference-color-li__img">
+                <img src="/src/img/difference-red2.jpg" alt="">
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</div>
