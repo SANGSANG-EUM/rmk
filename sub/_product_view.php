09719bdbb8f9117a4f1951f3c0010cda66c76a5d
onesick <onesick@iium.kr>
2024-03-12 13:31:39 +0900

    [제품상세] 상세내용영역 완료 (PC)

--- sub/_product_view.php
+++ sub/_product_view.php
@@ -161,12 +161,265 @@
       <div class="container">
         <div class="wrapper">
           <div class="inner">
-            <!-- ★★★★★개발시 imsi-box 삭제해주세요 -->
-            <div class="imsi-box" style="position: absolute; top
-            0; left: 0; z-index: 10; width: 100%; height: 100%; padding-top: 200px; background-color: rgba(0,0,0,0.3);"><span style="display: block; width: 200px; height: 70px; line-height: 70px; margin: 0 auto; background: red; font-size: 20px; font-weight: 800; color: #fff;">에디터 내용 영역</span></div>
 
-            <img src="/src/img/sunglass-detail.jpg" alt="">
-            <img src="/src/img/lens-detail.jpg" alt="">
+            <div class="sec-mt dalton-logo">
+              <img src="/src/img/dalton-logo.svg" alt="Dalton Logo">
+            </div>
+
+            <div class="sec-mt imgList">
+              <ul>
+                <li>
+                  <img src="/src/img/product-view-detail_img1.jpg" alt="">
+                </li>
+                <li>
+                  <img src="/src/img/product-view-detail_img2.jpg" alt="">
+                </li>
+                <li>
+                  <img src="/src/img/product-view-detail_img3.jpg" alt="">
+                </li>
+                <li>
+                  <img src="/src/img/product-view-detail_img4.jpg" alt="">
+                </li>
+                <li>
+                  <img src="/src/img/product-view-detail_img5.jpg" alt="">
+                </li>
+                <li>
+                  <img src="/src/img/product-view-detail_img6.jpg" alt="">
+                </li>
+              </ul>
+            </div>
+
+            <div class="sec-mt textList">
+              <p class="product-v_cTitle">처음 사용시 주의사항</p>
+              <div class="tContents tNumber">
+                <ul>
+                  <li>
+                    <div class="round-right num">01</div>
+                    <p class="tit">충분한 광량에서 사용하세요.</p>
+                    <div class="expl">돌튼 제품은 편안한 사용감을 위해 눈에 들어오는 빛의 양을 줄이는 코팅이 되어 있습니다. 밝은 실외에서 사용하시거나, 실내에서 사용하실 때는 조명을 밝게 해주세요.</div>
+                  </li>
+                  <li>
+                    <div class="round-left round-top-right num">02</div>
+                    <p class="tit">효과는 즉시~2주까지 걸릴 수 있습니다.</p>
+                    <div class="expl">돌튼의 효과는 대부분 착용 즉시 이전에 불가능했던 색상의 구분을 느낄 수 있습니다. 다만, 익숙하지 않은 색감때문에 적응에 약간의 시간이 걸릴 수 있습니다. 일부의 경우 효과를 느끼는데 2주까지 걸리기도 합니다.</div>
+                  </li>
+                  <li>
+                    <div class="round-top num">03</div>
+                    <p class="tit">자주 꼈다 벗었다 하지마세요.</p>
+                    <div class="expl">너무 자주 꼈다벗었다 하는 것은 눈이 새로운 렌즈에 적응하는데 어려움을 줄 수 있습니다. 가능한 한 번 착용하면 10분 이상 사용해주세요. 장시간 착용이 불편하면, 일반 선글라스처럼 돌튼이 필요한 순간만 착용할 수 있습니다.</div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="sec-mt sizeList">
+              <p class="product-v_cTitle">실측 사이즈</p>
+              <div class="size-img">
+                <img src="/src/img/product-size-pc.jpg" alt="" class="pc-view">
+                <img src="/src/img/product-size-mo.jpg" alt="" class="mo-view">
+              </div>
+              <p class="cau">* 실측 사이즈는 측정자와 측정방법에 따라 실제 사이즈와 약간의 오차가 발생할 수 있습니다.</p>
+            </div>
+
+            <div class="sec-mt lensType">
+              <p class="product-v_cTitle">모양 및 구조 - 치수</p>
+              <div class="type-ct">
+                <ul>
+                  <li>
+                    <div class="wrp">
+                      <p class="name">
+                        <span>오목형 (-)렌즈</span>
+                      </p>
+                      <div class="img">
+                        <img src="/src/img/lens-type-img1.jpg" alt="오목형렌즈 도움 이미지">
+                      </div>
+                      <div class="info">
+                        <ul>
+                          <li>
+                            <span class="tit">근시-난시</span>
+                            <span class="expl">0 ~ -8.00D</span>
+                          </li>
+                          <li>
+                            <span class="tit">치수</span>
+                            <span class="expl">55~80mm</span>
+                          </li>
+                          <li>
+                            <span class="tit">제품굴절율</span>
+                            <span class="expl">1.56</span>
+                          </li>
+                          <li>
+                            <span class="tit">재질</span>
+                            <span class="expl">플라스틱</span>
+                          </li>
+                        </ul>
+                      </div>
+                      <p class="cau">* 다만 이 경우 무도수 제품은 제외한다.</p>
+                    </div>
+                  </li>
+                  <li>
+                    <div class="wrp">
+                      <p class="name">
+                        <span>볼록형 (+)렌즈</span>
+                      </p>
+                      <div class="img">
+                        <img src="/src/img/lens-type-img1.jpg" alt="볼록형렌즈 도움 이미지">
+                      </div>
+                      <div class="info">
+                        <ul>
+                          <li>
+                            <span class="tit">근시-난시</span>
+                            <span class="expl">0 ~ +8.00D</span>
+                          </li>
+                          <li>
+                            <span class="tit">치수</span>
+                            <span class="expl">55~80mm</span>
+                          </li>
+                          <li>
+                            <span class="tit">제품굴절율</span>
+                            <span class="expl">1.56</span>
+                          </li>
+                          <li>
+                            <span class="tit">재질</span>
+                            <span class="expl">플라스틱</span>
+                          </li>
+                        </ul>
+                      </div>
+                      <p class="cau">* 다만 이 경우 무도수 제품은 제외한다.</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="sec-mt lensCont">
+              <div class="lt">
+                <div class="textList">
+                  <p class="product-v_cTitle">성능 소개</p>
+                  <div class="tContents">
+                    <ul>
+                      <li>
+                        <p class="tit">DALTON 1.56CC V2는 구면의 안경렌즈로 다음과 같은 성능을 지니고 있습니다.</p>
+                        <div class="expl">
+                          <ul class="tDp1">
+                            <li>1. 본 렌즈는 (-)렌즈시 0 ~ -8.00D, (+)렌즈 시 0 ~ -8.00D 성능을 지님</li>
+                            <li>2. 크기 55 ~ 80(mm), 5mm 단위 제조 가능함</li>
+                            <li>3. 굴절율 : 1.56</li>
+                            <li>4. 비중 : 1.28g/cm3 ~ 1.50g/cm3</li>
+                            <li>5. 아베수 : 32~58</li>
+                            <li>6. 능력 :
+                              <ul class="tDp2">
+                                <li>자외선 차단 UVA : 316~318nm 시 99.9%</li>
+                                <li>자외선 차단 UVB : 280~315nm 시 99.9%</li>
+                                <li>녹색 차단 : 520~530nm 시 95%</li>
+                              </ul>
+                            </li>
+                            <li>7. 기타(전 모델에 오목 및 볼록렌즈가 적용 가능, 아래 특수코팅이 가능함)</li>
+                          </ul>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="tableCont">
+                  <table class="ui-table">
+                    <thead>
+                      <tr>
+                        <th>코팅 종류</th>
+                        <th>성능</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>HMC</td>
+                        <td>렌즈의 표면을 강화하기 위하여 막을 형성하는 기본 하드 코팅</td>
+                      </tr>
+                      <tr>
+                        <td>SHMC</td>
+                        <td>HMC 코팅에 추가적으로 초발수 처리하는 코팅</td>
+                      </tr>
+                      <tr>
+                        <td>NIR</td>
+                        <td>코팅에 추가적으로 유해 근적외선을 차단하기 위한 코팅</td>
+                      </tr>
+                      <tr>
+                        <td>BlueSafe</td>
+                        <td>눈에 유해한 자외선 및 청색광을 차단하고 깨끗한 시야를 제공</td>
+                      </tr>
+                      <tr>
+                        <td>TN</td>
+                        <td>실외에서 자외선의 양에 따라 선글라스와 동일하게 색상이 짙어져 눈에 해로운 자외선을 차단하고 실내에서는 일반 안경과 동일하게 투명</td>
+                      </tr>
+                      <tr>
+                        <td>PO</td>
+                        <td>편광필름을 넣어 빛이 한 방향에서만 통과할 수 있도록 가공. 빛의 난반사를 막아 눈부심으로 인한 눈의 피로감을 덜어주고 더 맑은 시야를 제공</td>
+                      </tr>
+                      <tr>
+                        <td>CC</td>
+                        <td>530nm 대역을 포함한 가시광선 일부를 차단하여 적색과 녹색간 색구분을 용이하게 하여 적녹색약 보정효과가 있음. 코팅의 색상은 외측면은 적색 및 녹색 반사광, 내측면은 적색임.</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <div class="rt">
+                <div class="textList">
+                  <p class="product-v_cTitle">사용방법 및 주의사항</p>
+                  <div class="tContents">
+                    <ul>
+                      <li>
+                        <p class="tit">사용 전의 준비사항</p>
+                        <div class="expl">
+                          <ul class="tDp1">
+                            <li>1. 반드시 안과의사 또는 안경사에게 검안 후 사용할 것</li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li>
+                        <p class="tit">사용방법</p>
+                        <div class="expl">
+                          <ul class="tDp1">
+                            <li>1. 동공 중심에 렌즈를 정해진 위치에 맞추고, 안경테의 크기와 모양에 맞게 가공한다.</li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li>
+                        <p class="tit">사용 후 보관 및 관리방법</p>
+                        <div class="expl">
+                          <ul class="tDp1">
+                            <li>1. 직사광선이나 고온, 다습을 피하여 전용 케이스에 보관할 것</li>
+                            <li>2. 렌즈를 볼록한 부분이 위를 향하도록 보관할 것</li>
+                            <li>3. 안경렌즈 전용 천으로 감싼 후에 보관할 것</li>
+                            <li>4. 안경렌즈 전용 천(부드러운 천)을 사용하여 닦을 것</li>
+                            <li>5. 세척할 때 미지근한 물에 중성세제를 사용하여 닦을 것</li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li class="bd-top-dash">
+                        <p class="tit">사용 시 주의사항</p>
+                        <div class="expl">
+                          <ul class="tDp1">
+                            <li>1. 제품을 다룰 때는 렌즈의 테두리 부분을 잡을 것</li>
+                            <li>2. 렌즈의 밖과 안을 손으로 만지지 말 것</li>
+                            <li>3. 떨어트리지 말 것</li>
+                            <li>4. 열풍이나 고온에 닿지 않게 할 것</li>
+                            <li>5. 수면시 착용을 해제할 것</li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li>
+                        <p class="tit">보관 시 주의사항</p>
+                        <div class="expl">
+                          <ul class="tDp1">
+                            <li>1. 어린이의 손이 닿지 않도록 용기에 보관할 것</li>
+                            <li>2. 상온에서 보관할 것</li>
+                          </ul>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
